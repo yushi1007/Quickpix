2eaafc9761c611442ea38bb60da3345216e86883
Yu Shi <yushiys95@gmail.com>
2021-05-17 18:48:16 -0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,13 @@ palmtree = Image.create(name: "Palmtree", description: "Tree", price: 100, url: 
 frog = Image.create(name: "Frog", description: "Green frog", price: 80, url: "https://images.unsplash.com/photo-1545006398-2cf47cd87b90?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=915&q=80", user_id: 2)
 cheery_blossom = Image.create(name: "Cheery Blossom", description: "Flower", price: 60, url: "https://images.unsplash.com/photo-1614535675189-6649712cec83?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=962&q=80", user_id: 1)
 beach = Image.create(name: "Beach", description: "Aerial beach photo", price: 40, url: "https://images.unsplash.com/photo-1613307378200-0b366638a906?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80", user_id: 2)
+mountain = Image.create(name: "Mountain", description: "Reflection photo", price: 70, url: "https://images.unsplash.com/photo-1508923567004-3a6b8004f3d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80", user_id: 2)
+street = Image.create(name: "Street", description: "Rainning day", price: 100, url: "https://images.unsplash.com/photo-1531168795991-f0b78ed6796d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80", user_id: 1)
+bridge = Image.create(name: "Lower Manhattan", description: "On the Manhattan Bridge", price: 40, url: "https://images.unsplash.com/photo-1486671736870-2f695ecdf813?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80", user_id: 1)
+beach = Image.create(name: "Beach", description: "Aerial beach photo", price: 40, url: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=581&q=80", user_id: 2)
+tokyo = Image.create(name: "Tokyo Tower", description: "Nice view", price: 40, url: "https://images.unsplash.com/photo-1516292698305-5cba99c9f9c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80", user_id: 1)
+apple = Image.create(name: "Apple", description: "Still life", price: 50, url: "https://images.unsplash.com/photo-1588415986895-48daf87df9b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80", user_id: 1)
+leafs = Image.create(name: "Leafs", description: "Leafs", price: 60, url: "https://images.unsplash.com/photo-1600784499578-c62b50eded3f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=582&q=80", user_id: 2)
 
 
 #####TAG#####
